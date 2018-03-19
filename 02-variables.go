8a44c55683979f45ac43ce5d0574c21b43ba7a1a
Dipen-Dedania <dipen27891@gmail.com>
2018-03-19 19:11:21 +0530

    hello world

---
+++ 02-variables.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var b1, c1 float64 = 1.1, 2.1
+	fmt.Println(b1, c1)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println("e", e)
+
+	f := "short"
+	fmt.Println(f)
+
+	const n = 500000000
+	fmt.Println(n)
+
+	//n = n*5
+	//cannot assign to n
+
+	const n2 = 3e20 / n
+	fmt.Println(n2)
+	fmt.Println(int64(n2))
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # go-playground
 Sample programs with GO
+
+Download GO from -> https://golang.org/doc/install?download=go1.10.windows-386.msi
+
+## Hello World
+
+```
+go build 01-hello-world.go
+```
+
+This will create an .exe file. You can directly run go file by
+
+```
+go run 01-hello-world.go
+```
